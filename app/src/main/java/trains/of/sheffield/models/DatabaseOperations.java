ad87974815cee6ee06c2f21e853e96c515a96320
Krish Panchigar <krishpanchigar14@gmail.com>
2023-11-21 20:09:50 +0000

    add hashing generators

--- app/src/main/java/trains/of/sheffield/models/DatabaseOperations.java
+++ app/src/main/java/trains/of/sheffield/models/DatabaseOperations.java
@@ -1,10 +1,9 @@
 package trains.of.sheffield.models;
 
 import trains.of.sheffield.*;
+import trains.of.sheffield.util.HashedPasswordGenerator;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabaseOperations {
 
@@ -35,5 +34,62 @@ public class DatabaseOperations {
             GUILoader.alertWindow("Error: Could not connect "+ex); // Outputs error message
         }
         return false;
+    }
+
+    public String verifyLogin(Connection connection, String username, char[] enteredPassword) {
+        try {
+            // Query the database to fetch user information
+            String sql = "SELECT userId, password_hash, failed_login_attempts, " +
+                    "account_locked FROM Users WHERE username = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, username);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                String userId = resultSet.getString("userId");
+                String storedPasswordHash = resultSet.getString("password_hash");
+                int failedLoginAttempts = resultSet.getInt("failed_login_attempts");
+                boolean accountLocked = resultSet.getBoolean("account_locked");
+
+                // Check if the account is locked
+                if (accountLocked) {
+                    return "Account is locked. Please contact support.";
+                } else {
+                    // Verify the entered password against the stored hash
+                    if (verifyPassword(enteredPassword, storedPasswordHash)) {
+                        // Update the last login time
+                        sql = "UPDATE Users SET last_login = CURRENT_TIMESTAMP, " +
+                                "failed_login_attempts = 0 WHERE userId = ?";
+                        statement = connection.prepareStatement(sql);
+                        statement.setString(1, userId);
+                        statement.executeUpdate();
+                        return "Login successful for user: " + username;
+                    } else {
+                        // Incorrect password, update failed login attempts
+                        failedLoginAttempts++;
+                        sql = "UPDATE Users SET failed_login_attempts = ? WHERE userId = ?";
+                        statement = connection.prepareStatement(sql);
+                        statement.setInt(1, failedLoginAttempts);
+                        statement.setString(2, userId);
+                        statement.executeUpdate();
+
+                        return "Incorrect password. Failed login attempts: " + failedLoginAttempts;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "User not found.";
+    }
+
+    private static boolean verifyPassword(char[] enteredPassword, String storedPasswordHash) {
+        try {
+            String hashedEnteredPassword = HashedPasswordGenerator.hashPassword(enteredPassword);
+            return hashedEnteredPassword.equals(storedPasswordHash);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
