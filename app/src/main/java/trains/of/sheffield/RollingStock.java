4f4ee72df92a64c5dd503097380498d0c771f306
Krish Panchigar <krishpanchigar14@gmail.com>
2023-11-22 14:29:39 +0000

    add constructors

--- app/src/main/java/trains/of/sheffield/RollingStock.java
+++ app/src/main/java/trains/of/sheffield/RollingStock.java
@@ -1,4 +1,8 @@
 package trains.of.sheffield;
 public class RollingStock extends Product {
     private String era;
+
+    public RollingStock(String productCode, String brandName, String productName, Double price, Gauge gauge, String description, Integer stock) {
+        super(productCode, brandName, productName, price, gauge, description, stock);
+    }
 }
